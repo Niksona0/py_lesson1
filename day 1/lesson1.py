62250a9e0bb618ffa432dad98a83acb070baeeb3
class1cal <nikaarudashviliyt2008@gmail.com>
2022-09-21 16:06:44 +0400

    Learned how to create and print out variables in py.

---
+++ day 1/lesson1.py
@@ -0,0 +1,8 @@
+name = "Nika" # სახელი - Name
+surname = "Arudashvili" # გვარი - Surname
+age = 14 # ასაკი - Age
+height = 175.3 # სიმაღლე - height
+school_grade = 9 # კლასი - Grade
+hobbie = "programming, chess, cubing and gaming" # ჰობი - Hobbie
+
+print("My name is " + name + " " + surname + ". I am " + str(age) + " years old. I am in " + str(school_grade) + "th grade and my height is " + str(height) + "cm. I love " + hobbie + ".")
